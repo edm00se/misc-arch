8aa4f2b09dc73d48052a9732248fffd72ef1e2d3
Eric McCormick <edm00se@gmail.com>
2018-05-18 14:24:05 -0500

    re-instantiates with vuepress-clean theme
    not using as sub-module, but copied as of 16b0507, customizations coming

---
+++ .vuepress/config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+  title: '…',
+  description: 'a place for miscellanea',
+  themeConfig: {
+    repo: 'edm00se/misc',
+    docsRepo: 'edm00se/misc',
+    editLinks: false,
+
+    // custom property to show links about you
+    usefulLinks: [
+      {
+        href: 'https://twitter.com/edm00se',
+        cssIcon: 'fa fa-fw fa-twitter'
+      },
+      {
+        href: 'https://stackoverflow.com/cv/edm00se',
+        cssIcon: 'fa fa-fw fa-stack-overflow'
+      },
+      {
+        href: 'https://github.com/edm00se',
+        cssIcon: 'fa fa-fw fa-github'
+      },
+      {
+        href: 'https://www.linkedin.com/in/emccormick',
+        cssIcon: 'fa fa-fw fa-linkedin'
+      }
+    ]
+  },
+
+  // This allows to install local loaders for the custom theme (eg: sass-loader, less-lodaer)
+  chainWebpack: (config, isServer) => {
+    config.resolveLoader
+      .modules
+      .add(path.resolve(__dirname, './node_modules'))
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+---
+home: true
+---
+
+# Miscellanea
+
+> a collection of miscellaneous objects or writings
+>
+> -- <cite>[Merriam-Webster](https://www.merriam-webster.com/dictionary/miscellanea)</cite>
